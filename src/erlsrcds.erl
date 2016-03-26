cb73ebbb46d3aab494127653ecfe7f6cdc201060
Kristoffer Dalby <kradalby@kradalby.no>
2016-03-26 21:26:38 +0100

    defines macros for payloads

--- src/erlsrcds.erl
+++ src/erlsrcds.erl
@@ -1,1 +1,16 @@
 -module(erlsrcds).
+
+
+-define(PACKETSIZE, 1400).
+-define(WHOLE, -1).
+-define(SPLIT, -2).
+
+-define(A2S_INFO, 84).
+-define(A2S_INFO_STRING, "Source Engine Query").
+-define(A2S_INFO_REPLY, 73).
+-define(A2S_PLAYER, 85).
+-define(A2S_PLAYER_REPLY, 68).
+-define(A2S_RULES, 86).
+-define(A2S_RULES_REPLY, 69).
+-define(CHALLENGE, -1).
+-define(S2C_CHALLENGE, 65).
