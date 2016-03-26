0bc33883a1f15d07690931c572278f24fde10b84
Kristoffer Dalby <kradalby@kradalby.no>
2016-03-26 21:34:41 +0100

    changes macros to correct erlang syntax

--- src/erlsrcds.erl
+++ src/erlsrcds.erl
@@ -5,12 +5,12 @@
 -define(WHOLE, -1).
 -define(SPLIT, -2).
 
--define(A2S_INFO, 84).
+-define(A2S_INFO, <<"T">>).
 -define(A2S_INFO_STRING, "Source Engine Query").
--define(A2S_INFO_REPLY, 73).
--define(A2S_PLAYER, 85).
--define(A2S_PLAYER_REPLY, 68).
--define(A2S_RULES, 86).
--define(A2S_RULES_REPLY, 69).
+-define(A2S_INFO_REPLY, <<"I">>).
+-define(A2S_PLAYER, <<"U">>).
+-define(A2S_PLAYER_REPLY, <<"D">>).
+-define(A2S_RULES, <<"V">>).
+-define(A2S_RULES_REPLY, <<"E">>).
 -define(CHALLENGE, -1).
--define(S2C_CHALLENGE, 65).
+-define(S2C_CHALLENGE, <<"A">>).
