9433fd70bf3a0b6d3358da69f1f7757942d0cbf0
Kristoffer Dalby <kradalby@kradalby.no>
2016-03-29 19:12:42 +0200

    adds some basic query tests

--- apps/erlsrcds/test/erlsrcds_tests.erl
+++ apps/erlsrcds/test/erlsrcds_tests.erl
@@ -18,3 +18,19 @@ rules_timeout_test() ->
     Port = 27015,
     Result = erlsrcds:rules(Address, Port),
     ?assertEqual({error, timeout}, Result).
+
+info_query_test() ->
+    Address = "193.202.115.74",
+    Port = 27015,
+    Result = erlsrcds:info(Address, Port),
+    Hostname = "Recess.no #01 Bhop [ Easy | Timer | Ranking | Replay Bots ]",
+    FetchedHostname = maps:get("hostname", Result),
+    ?assertEqual(Hostname, FetchedHostname).
+
+rules_query_test() ->
+    Address = "193.202.115.74",
+    Port = 27015,
+    Result = erlsrcds:rules(Address, Port),
+    Key = "sv_pausable",
+    Value = maps:get(Key, Result),
+    ?assertEqual(Value, "0").
