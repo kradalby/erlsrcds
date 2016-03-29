e731ea3cb3bfa339a4def8227a91661c05383443
Kristoffer Dalby <kradalby@kradalby.no>
2016-03-29 19:06:32 +0200

    fixes tests

--- apps/erlsrcds/test/erlsrcds_tests.erl
+++ apps/erlsrcds/test/erlsrcds_tests.erl
@@ -5,16 +5,16 @@ info_timeout_test() ->
     Address = "8.8.8.8",
     Port = 27015,
     Result = erlsrcds:info(Address, Port),
-    ?_assert({error, timeout} =:= Result).
+    ?assertEqual({error, timeout}, Result).
 
 player_timeout_test() ->
     Address = "8.8.8.8",
     Port = 27015,
     Result = erlsrcds:player(Address, Port),
-    ?_assert({error, timeout} =:= Result).
+    ?assertEqual({error, timeout}, Result).
 
 rules_timeout_test() ->
     Address = "8.8.8.8",
     Port = 27015,
     Result = erlsrcds:rules(Address, Port),
-    ?_assert({error, timeout} =:= Result).
+    ?assertEqual({error, timeout}, Result).
