8e6846b200661aa9298ff01d8a6b349241466215
Kristoffer Dalby <kradalby@kradalby.no>
2016-05-12 22:45:01 +0200

    the punishment of using online servers

--- test/erlsrcds_tests.erl
+++ test/erlsrcds_tests.erl
@@ -3,33 +3,33 @@
 
 info_timeout_test() ->
     Address = "8.8.8.8",
-    Port = 27015,
+    Port = 27115,
     Result = erlsrcds:info(Address, Port),
     ?assertEqual({error, timeout}, Result).
 
 player_timeout_test() ->
     Address = "8.8.8.8",
-    Port = 27015,
+    Port = 27115,
     Result = erlsrcds:player(Address, Port),
     ?assertEqual({error, timeout}, Result).
 
 rules_timeout_test() ->
     Address = "8.8.8.8",
-    Port = 27015,
+    Port = 27115,
     Result = erlsrcds:rules(Address, Port),
     ?assertEqual({error, timeout}, Result).
 
 info_query_test() ->
-    Address = "193.202.115.74",
-    Port = 27015,
+    Address = "193.202.115.85",
+    Port = 27115,
     Result = erlsrcds:info(Address, Port),
-    Hostname = "Recess.no #01 Bhop [ Easy | Timer | Ranking | Replay Bots ]",
+    Hostname = "Recess.no #12 Surf [ Tier 1-2 | Timer | 100 Tick ]",
     FetchedHostname = maps:get("hostname", Result),
     ?assertEqual(Hostname, FetchedHostname).
 
 rules_query_test() ->
-    Address = "193.202.115.74",
-    Port = 27015,
+    Address = "193.202.115.85",
+    Port = 27115,
     Result = erlsrcds:rules(Address, Port),
     Key = "sv_pausable",
     Value = maps:get(Key, Result),
