8e22c044163133caadb9529372c24eeda4ef4375
Kristoffer Dalby <kradalby@kradalby.no>
2016-05-12 22:49:48 +0200

    csgo does not have rules public

--- test/erlsrcds_tests.erl
+++ test/erlsrcds_tests.erl
@@ -27,10 +27,10 @@ info_query_test() ->
     FetchedHostname = maps:get("hostname", Result),
     ?assertEqual(Hostname, FetchedHostname).
 
-rules_query_test() ->
-    Address = "193.202.115.82",
-    Port = 27115,
-    Result = erlsrcds:rules(Address, Port),
-    Key = "sv_pausable",
-    Value = maps:get(Key, Result),
-    ?assertEqual(Value, "0").
+% rules_query_test() ->
+%     Address = "193.202.115.82",
+%     Port = 27115,
+%     Result = erlsrcds:rules(Address, Port),
+%     Key = "sv_pausable",
+%     Value = maps:get(Key, Result),
+%     ?assertEqual(Value, "0").
