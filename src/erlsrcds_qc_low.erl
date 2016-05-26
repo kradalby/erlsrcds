a942f3554fbcb28c28147b97bedc412ed054d61a
Kristoffer Dalby <kradalby@kradalby.no>
2016-05-26 14:58:29 +0200

    initial commit of the statemachine for lowlevel testing

---
+++ src/erlsrcds_qc_low.erl
@@ -0,0 +1,17 @@
+-module(erlsrcds_qc_low).
+
+-compile(export_all).
+
+-include_lib("eqc/include/eqc.hrl").
+-include_lib("eqc/include/eqc_statem.hrl").
+
+
+-define(SERVER, "127.0.0.1").
+-define(PORT, 27015).
+-define(PASSWORD, "passwrd").
+
+-record(state, {
+}).
+
+initial_state() ->
+    #state{}.
